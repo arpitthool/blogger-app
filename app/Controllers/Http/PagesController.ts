aba518163fe0477794af861c039d618c6a1cea0e
arpitthool <arpitthool1@gmail.com>
2023-11-09 19:39:13 -0500

    app/Controllers/Http/PagesController.ts: removed unecessary commented code; resources/views/layouts/partials/_navbar.edge: commneted out links in navbar : about, contact, greeting

--- app/Controllers/Http/PagesController.ts
+++ app/Controllers/Http/PagesController.ts
@@ -22,33 +22,17 @@ export default class PagesController {
     }
 
     // method to render contact page
-    /*
-    public contact () {
-        return 'This is the contact page'
-    }
-    */
-    // Render views instead of returning string
     public contact ( { view } : HttpContextContract) {
         return view.render('contact')
     }
 
     // method to render hello page using the 'name' param passed
-    /*
-    public hello ( { params } : HttpContextContract ) {
-        return `Hello ${params.name}, hope your day is going well :)`
-    }
-    */
     public hello ( { view, params } : HttpContextContract ) {
         const name = params.name
         return view.render('hello', { name }) // passing name variable to the view
     }
 
     // method to render greeting page using the 'name' param passed
-    /*
-    public greeting ( { params } : HttpContextContract ) {
-        return params.name ? `Greetings ${params.name}! hope your day is going well :)` : `Greetings user! hope your day is going well :)`
-    }
-    */
     public greeting ( { view, params } : HttpContextContract ) {
         const name = params.name
         return view.render('greeting', { name })
