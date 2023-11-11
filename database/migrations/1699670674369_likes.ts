9b48a0576c722a8173329da05d61d469f1121b92
arpitthool <arpitthool1@gmail.com>
2023-11-11 03:24:09 -0500

    created likes table to store likes based on user id and blog id

---
+++ database/migrations/1699670674369_likes.ts
@@ -0,0 +1,22 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'likes'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').notNullable()
+      table.integer('blog_id').notNullable()
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
