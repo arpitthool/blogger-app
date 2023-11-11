98ff5c4ac94cdc879142704a4c170db4481bafbc
arpitthool <arpitthool1@gmail.com>
2023-11-11 03:26:53 -0500

    app/Models/Like.ts: created Like Model and defined belongs to relationship with user and blog

---
+++ app/Models/Like.ts
@@ -0,0 +1,29 @@
+import { DateTime } from 'luxon'
+import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import Blog from './Blog'
+import User from './User'
+
+export default class Like extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public userId: number
+
+  @column()
+  public blogId: number
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  // RELATIONSHIP : one like can only have one blog
+  @belongsTo(() => Blog)
+  public blog: BelongsTo<typeof Blog>
+
+  // RELATIONSHIP : one like can only have one user
+  @belongsTo(() => User)
+  public user: BelongsTo<typeof User>
+}
