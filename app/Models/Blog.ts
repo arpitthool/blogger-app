340409f4ee3cbea28ebfc27100749e8fa3111bee
arpitthool <arpitthool1@gmail.com>
2023-11-11 03:28:14 -0500

    app/Models/Blog.ts: modified Blog model: added totalLikes column; defined hasMany relationship with Like

--- app/Models/Blog.ts
+++ app/Models/Blog.ts
@@ -1,6 +1,7 @@
 import { DateTime } from 'luxon'
-import { BaseModel, BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, BelongsTo, HasMany, belongsTo, column, hasMany } from '@ioc:Adonis/Lucid/Orm'
 import User from './User'
+import Like from './Like'
 
 export default class Blog extends BaseModel {
   @column({ isPrimary: true })
@@ -15,12 +16,20 @@ export default class Blog extends BaseModel {
   @column()
   public content: string
 
+  @column()
+  public totalLikes: number
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
 
+  // RELATIONSHIP : Blog can have only one author
   @belongsTo(() => User)
   public user: BelongsTo<typeof User>
+
+  // RELATIONSHIP : one-to-many : Blog can have many likes
+  @hasMany(() => Like)
+  public likes: HasMany<typeof Like>
 }
