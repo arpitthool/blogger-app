91ee0d43c9c3838cad6d96fd1b1c61a5ad2dabf4
arpitthool <arpitthool1@gmail.com>
2023-11-13 11:52:38 -0500

    frontend/src/app.html frontend/src/routes/+page.svelte : set bulma css stylesheet; created index route to fetch and show all the blogs from backend

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,75 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<script>
+    import { onMount } from 'svelte';
+    import axios from 'axios';
+  
+    /**
+     * @type {any[]}
+     */
+    let blogs = [];
+  
+    onMount(async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:3333/api');
+        console.log(response.data)
+        blogs = response.data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    });
+  </script>
+  
+  <div class="columns">
+    <div class="coulmn is-three-fifths is-offset-one-fifth">
+        
+        <!-- diaplay existing blogs   -->
+        <div class="container">
+            <div class="box">
+                <div class="columns is-multiline">
+
+                    <!-- iterate over blogs -->
+                    {#each blogs as blog}
+
+                    <div class="column" style="min-width: 300px;">
+                        <div class="card">
+
+                            <header class="card-header">
+                                <p class="card-header-title">
+                                    <a href="{`/blog/${blog.id}`}">{blog.title }</a>
+                                </p>
+
+                            </header>
+
+                            <div class="card-content">
+                                <div class="content">
+                                    { blog.content }
+                                </div>
+                            </div>
+
+                            <!--  card footer showing total likes on blog, like button and dislike button -->
+                            <footer class="card-footer">
+                                    
+                                    <span class="card-footer-item"> &#128525; { blog.total_likes }</span>
+                                    <a class="card-footer-item" href="/likeBlog/{blog.id }">&#128077;</a>
+                                    <a class="card-footer-item" href="/dislikeBlog/{blog.id }">&#128078;</a>
+                                
+                            </footer>
+
+                        </div>
+                    </div>
+
+                    <!-- {{--  if no blogs exist : show message  --}} -->
+                    {:else}
+                    <div class="column">
+                        <div class="notification is-warning">
+                            No blogs yet!
+                        </div>
+                    </div>
+
+                    {/each}
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
