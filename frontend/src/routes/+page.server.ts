29df78e5e8a66637ec377736349f802d2d2ab9bb
arpitthool <arpitthool1@gmail.com>
2024-03-22 00:30:51 -0400

    frontend/src/routes/+page.server.ts: refactored the API endpoint to fetch all blogs

--- frontend/src/routes/+page.server.ts
+++ frontend/src/routes/+page.server.ts
@@ -1,7 +1,7 @@
 import { BACKEND_API } from '$env/static/private'
 
 export async function load({fetch}) {
-    const response = await fetch(`${BACKEND_API}`);
+    const response = await fetch(`${BACKEND_API}/blogs`);
     const blogs : Blog[] = await response.json();
     return {
         blogs
