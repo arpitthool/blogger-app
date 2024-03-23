b476482072091010431a5c5e4baed3fa15af73de
arpitthool <arpitthool1@gmail.com>
2024-03-23 19:12:01 -0400

    initial try using nginx

--- frontend/src/routes/blogs/+page.server.ts
+++ frontend/src/routes/blogs/+page.server.ts
@@ -1,4 +1,6 @@
-import { BACKEND_API } from '$env/static/private'
+// import { BACKEND_API } from '$env/static/private'
+
+let BACKEND_API="127.0.0.1:3333/api"
 
 export async function load({fetch}) {
     const response = await fetch(`${BACKEND_API}/blogs`);
